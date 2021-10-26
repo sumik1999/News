4201c0d7a746d8f2d9d71fe9d8d8e4aef3ee27af
sumik1999 <kiran1999rp@gmail.com>
2021-10-26 07:37:55 +0530

    email backend added

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -131,4 +131,10 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
